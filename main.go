2d83f91491e11f77918557aa4c25be30a70dbf7b
vrima25 <vrimaratna@gmail.com>
2026-08-04 14:47:28 +0700

    add cors handler

--- main.go
+++ main.go
@@ -51,7 +51,24 @@ func main(){
 	)
 
 	log.Println("server running on :8080")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+
+	handler := corsMiddleware(mux)
+	if err := http.ListenAndServe(":8080", handler); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
